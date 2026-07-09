3de34bfef0533e4f6b2d962ae10b900d6dd00ed6
Edward <lizarese@uci.edu>
2026-07-08 23:12:06 -0700

    Finished Day 1 exercises

---
+++ 01_Day_Introduction/exercise_level_3.py
@@ -0,0 +1,8 @@
+print("Float: 1.01")
+print('String: "This is a string"')
+print("Boolean: True")
+print("List: [1,2,3]")
+print("Tuple: (1,2,3)")
+print("Set: {1,2,3}")
+print("Dictionary: {1:a, 2:b, 3:c}")
+print(((10-2)**2+(8-3)**2)**(1/2))
