10d6ffd12bc9181efa8cffd884a04b04e67f8d9f
Edward <lizarese@uci.edu>
2026-07-08 23:14:03 -0700

    Added github helper string

--- 01_Day_Introduction/exercise_level_3.py
+++ 01_Day_Introduction/exercise_level_3.py
@@ -6,3 +6,10 @@ print("Tuple: (1,2,3)")
 print("Set: {1,2,3}")
 print("Dictionary: {1:a, 2:b, 3:c}")
 print(((10-2)**2+(8-3)**2)**(1/2))
+
+"""
+Github Helper
+git add .
+git commit -m "Message"
+git push origin master
+"""
