06dabf0075cc05d9b7d9db2cbadfebcf386a48d5
Edward <lizarese@uci.edu>
2026-07-09 10:04:10 -0700

    Completed Day 17 Exercises

---
+++ 17_Day_Exception_handling/Exercises/exercises.py
@@ -0,0 +1,4 @@
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+*nordic_countries, es, ru = names # i thought you had to reverse, nice!
+print("1:", nordic_countries, es, ru)
