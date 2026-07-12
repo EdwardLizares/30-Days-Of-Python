5cacd2468ec76fbaa7465fbe16f307f389859da7
Edward <lizarese@uci.edu>
2026-07-12 07:58:54 -0700

    Completed Day 25 Exercises

---
+++ 25_Day_Pandas/Exercises/exercises.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import csv
+
+def open_hacker():
+    with open('data\hacker_news.csv', 'r', encoding='utf-8') as f:
+        d = [l for l in csv.reader(f, delimiter=',')]
+    return pd.DataFrame(d[1:], columns=d[0])
+
+if __name__ == "__main__":
+    data = open_hacker()
+    print("2:", data[:5])
+    print("3:", data[-5:])
+    print("4:", data["title"])
+    print("5:", data.shape)
+    print("5.1:", data["title"][data["title"].str.contains("Python", case=False, na=False)])
---
+++ 25_Day_Pandas/Exercises/samples.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+data = [
+    {"Name": "Asabeneh", "Country":"Finland","City":"Helsinki"},
+    {"Name": "David", "Country":"UK","City":"London"},
+    {"Name": "John", "Country":"Sweden","City":"Stockholm"}]
+df = pd.DataFrame(data)
+print(df)
